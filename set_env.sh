0b7fd46daf234bcee0d581276d52761e5436903d
William <william.votn@gmail.com>
2023-01-06 02:47:04 +0100

    end part 2

--- set_env.sh
+++ set_env.sh
@@ -2,11 +2,11 @@
 
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=admin
-export POSTGRES_PASSWORD=admin1234
-export POSTGRES_HOST=udagramd-db
-export POSTGRES_DB=udagramdb
-export AWS_BUCKET=udagram-esiea-8765
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=password
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-esiea-1716
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
